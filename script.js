8bbc1f76360f47a15e6046cc3a990a6e19dc7095
luisdiko14-lab <luisdiko14@gmail.com>
2025-11-23 13:48:54 +0100

    Add download button functionality in script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("downloadBtn").addEventListener("click", function() {
+    window.location.href = "https://github.com/luisdiko14-lab/Ps1-and-Bat-files/tree/main/Cool%20Folder%20With%20Files";
+});
