c0e35c4381f9f75529a95a1b43ac78e765e5ae8a
luis <luisdiko14@gmail.com>
2025-12-10 19:54:32 +0100

    Update button event listeners in script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-// First button
+// Download button
 document.getElementById("downloadBtn").addEventListener("click", function() {
     window.location.href = "https://github.com/luisdiko14-lab/Ps1-and-Bat-files-and-more";
 });
 
-// Second button
-document.getElementById("viewRepo").addEventListener("click", function() {
+// View repo button
+document.getElementById("viewRepoBtn").addEventListener("click", function() {
     window.location.href = "https://github.com/luisdiko14-lab/Ps1-and-Bat-files/tree/main/Cool%20Folder%20With%20Files";
+});
+
+// Compatibility button
+document.getElementById("compatibility").addEventListener("click", function() {
+    window.location.href = "https://github.com/luisdiko14-lab/Ps1-and-Bat-files-and-more/blob/main/compatibility.md";
 });
