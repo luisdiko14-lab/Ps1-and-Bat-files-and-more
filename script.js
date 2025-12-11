5837692673d7bb4940f9139e52a529ef1d9c31ff
luis <luisdiko14@gmail.com>
2025-12-11 09:51:20 +0100

    Fix event listener for view repo button

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById("downloadBtn").addEventListener("click", function() {
 });
 
 // View repo button
-document.getElementById("viewRepoBtn").addEventListener("click", function() {
+document.getElementById("viewRepo").addEventListener("click", function() {
     window.location.href = "https://github.com/luisdiko14-lab/Ps1-and-Bat-files/tree/main/Cool%20Folder%20With%20Files";
 });
 
